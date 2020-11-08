0ff0ca99fdfcf21e3eba464e6a2cd22c376c4806
Jesús González <jgonzalez@gdr-sistemas.com>
2020-11-08 20:43:24 +0100

    Fixed error in CI test script when no tests are executed

--- ci/appveyor_ci_test.ps1
+++ ci/appveyor_ci_test.ps1
@@ -10,31 +10,37 @@ function Publish-TestResults($files)
     $anyFailures = $FALSE
 
     # Upload results to AppVeyor one by one
-    $files | foreach {
-        Write-Host "Analyzing test results file '$_'" 
+    foreach ($file in $files)
+    {
+        Write-Host "Analyzing test results file '$file'" 
     
-        $testsuite = ([xml](get-content $_)).testsuite
+        $testsuite = ([xml](get-content $file)).testsuite
 
-        foreach ($testcase in $testsuite.testcase) {
+        foreach ($testcase in $testsuite.testcase)
+        {
             [double]$testtimef = $testcase.time
             [int]$testtimei = $testtimef * 1000
-            if ($testcase.failure) {
+            if ($testcase.failure)
+            {
                 Add-AppveyorTest $testcase.name -Outcome Failed -FileName $testsuite.name -ErrorMessage $testcase.failure.message -Duration $testtimei
                 Add-AppveyorMessage "$($testcase.name) failed" -Category Error
                 $anyFailures = $TRUE
             }
-            elseif ($testcase.skipped) {
+            elseif ($testcase.skipped)
+            {
                 Add-AppveyorTest $testcase.name -Outcome Ignored -Filename $testsuite.name
             }
-            else {
+            else
+            {
                 Add-AppveyorTest $testcase.name -Outcome Passed -FileName $testsuite.name -Duration $testtimei
             }
         }
 
-        Remove-Item $_
+        Remove-Item $file
     }
 
-    if ($anyFailures -eq $TRUE){
+    if ($anyFailures -eq $TRUE)
+    {
         Write-Host -ForegroundColor Red "Failing build as there are broken tests"
         $host.SetShouldExit(1)
     }
