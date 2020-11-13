d9f08f6680ab4ffc41f22096661b494661d35a3d
Jesús González <jgonzalez@gdr-sistemas.com>
2020-11-13 15:26:18 +0100

    Updated CppUTest CMake find script to create an import library

--- test/GenerateTest.cmake
+++ test/GenerateTest.cmake
@@ -10,29 +10,13 @@ set( CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/"
 
 find_package( CppUTest REQUIRED )
 
-include_directories( ${CppUTest_INCLUDE_DIR} )
-link_directories( ${CppUTest_LIBRARY_DIR} )
-
-add_definitions( -DCPPUTEST_INCLUDE_DIR="${CppUTest_INCLUDE_DIR}" )
-add_definitions( -DCPPUMOCKGEN_INCLUDE_DIR="${CMAKE_SOURCE_DIR}/prod/include" )
-
 if( MSVC )
     set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc" )
 endif()
 
 add_executable( ${PROJECT_NAME} EXCLUDE_FROM_ALL ${PROD_SRC_FILES} ${TEST_SRC_FILES} ${CMAKE_CURRENT_LIST_DIR}/TestMain.cpp )
 
-if( DEFINED CppUTest_DEBUG_LIBRARY )
-    target_link_libraries( ${PROJECT_NAME} debug ${CppUTest_DEBUG_LIBRARY} optimized ${CppUTest_LIBRARY} )
-    if( DEFINED CppUTestExt_DEBUG_LIBRARY )
-        target_link_libraries( ${PROJECT_NAME} debug ${CppUTestExt_DEBUG_LIBRARY} optimized ${CppUTestExt_LIBRARY} )
-    endif()
-else()
-    target_link_libraries( ${PROJECT_NAME} ${CppUTest_LIBRARY} )
-    if( DEFINED CppUTestExt_LIBRARY )
-        target_link_libraries( ${PROJECT_NAME} ${CppUTestExt_LIBRARY} )
-    endif()
-endif()
+target_link_libraries( ${PROJECT_NAME} ${CppUTest_LIBRARIES} )
 
 add_dependencies( build_tests ${PROJECT_NAME} )
 
